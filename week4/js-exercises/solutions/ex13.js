d058a98ac143ce545acc73b6d66ce73b396f5c71
Mayu <mayu.i.19930327@gmail.com>
2023-02-15 14:07:04 -0800

    first commit

--- week4/js-exercises/solutions/ex13.js
+++ week4/js-exercises/solutions/ex13.js
@@ -1,1 +1,9 @@
 // write your solution here
+
+function factrial(n) {
+    let j = 1;
+    for(let i = 1; i <= n; i++) {
+        j = j * i;
+        return j;
+    }
+}
--- week4/js-exercises/solutions/ex9.js
+++ week4/js-exercises/solutions/ex9.js
@@ -1,1 +1,28 @@
 // write your solution here
+
+let count = 60;
+while (count >= 0) {
+    switch (count) {
+        case 50:
+            console.log("Orbiter transfers from ground to internal power");
+            break;
+        case 31:
+            console.log("Ground launch sequencer is go for auto sequence start");
+            break;
+        case 16:
+            console.log("Activate launch pad sound suppression system");
+            break;
+        case 10:
+            console.log("Activate main engine hydrogen burnoff system");
+            break;
+        case 6:
+            console.log("Main engine start");
+            break;
+        case 0:
+            console.log("Solid rocket booster ignition and liftoff!");
+            break;
+        default:
+            console.log("T-" + count + " seconds");
+    };
+    count--;
+};
