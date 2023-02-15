cc37761fc6d3b7a2ba0f84a8b7fedea0bb4001e1
Shiori <shiori.fujii.cb@gmail.com>
2023-02-15 09:52:08 -0800

    done web-dev-2 weeek4 js-lab-questions

--- week4/js-exercises/solutions/ex14.js
+++ week4/js-exercises/solutions/ex14.js
@@ -1,1 +1,13 @@
 // write your solution here
+const rows = 26;
+const seats = 100;
+
+function printSeats(rows,seats) {
+    for (let row = 0; row < rows; row++){
+        for (let seat = 0; seat < seats; seat++) {
+            console.log(`row: ${row} - seat: ${seat}`);
+        }
+    }
+}
+
+printSeats(rows, seats);
